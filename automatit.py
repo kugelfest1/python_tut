3220156fb59ae4bd3f8c612cda8e575a4252ba47
ed kan <kan1010@freenet.de>
2021-02-14 15:22:16 +0100

    upd

--- automatit.py
+++ automatit.py
@@ -1,3 +1,6 @@
+# automatit
+# 2021-02-14, created
+
 import requests
 
 def test():
@@ -5,4 +8,16 @@ def test():
   print("Response object:", r)
   print("Response Text:", r.text)
 
-test();
+
+def test2():
+  payload = {'q': 'chetan'}
+  r = requests.get('https://github.com/search', params=payload)
+  print("Request URL:", r.url)
+
+def test3():
+  payload = {'key1': 'value1'}
+  r = requests.post("http://httpbin.org/post", data=payload)
+  print("Response text:", r.json())
+
+
+test3()
