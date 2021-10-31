f02a04389bd546771120b0988a7f1dfe010f0557
ed kan <kan1010@freenet.de>
2021-10-31 18:23:55 +0100

    upd

--- mymath.py
+++ mymath.py
@@ -2,12 +2,15 @@
 # 28.10.21 created
 from random import randrange as rr
 def test():
-    op=('+','-','*')
+    op=('+','-','*','//')
     for i in range(0,10):
-        eq='%d %c %d %c %d %c %d' %(rr(10), op[rr(3)], rr(10), op[rr(3)], rr(10), op[rr(3)], rr(10))
-        res=input('What is %s ? (<enter> to exit) ' %(eq))
-        if res=='': break
-        print('Correct' if abs(eval(eq)-int(res))<0.1 else 'Wrong, the correct answer is %d' %(eval(eq)))
+        eq='%d %s %d %s %d %s %d' %(rr(10), op[rr(4)], rr(10), op[rr(4)], rr(10), op[rr(4)], rr(10))
+        try:
+          res=input('What is %s ? (-1 to exit) ' %(eq))
+          if res=='-1': break
+          print('Correct' if int(eval(eq))==int(res) else 'Wrong, the correct answer is %d' %(eval(eq)))
+        except:
+          print('Something went wrong, try again (-1 to exit)')
     return None
 
 test()
