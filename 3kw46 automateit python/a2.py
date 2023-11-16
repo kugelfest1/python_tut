55e8688ca3cdacd83f5e22f30545d481deb1e5bf
Edward Kan <kan1010@freenet.de>
2023-11-16 13:21:40 +0100

    upd

--- 3kw46 automateit python/a2.py
+++ 3kw46 automateit python/a2.py
@@ -12,4 +12,16 @@ def test2():
     # Making the soup
     print('Beautifulsoup object:', type(soup))    
 
+    #find elemsnts by tags
+    print(soup.find_all('a'))
+    print(soup.find_all('strong'))
+    # find elements by id
+    print(soup.find('div',{"id":"inventor"}))
+    print(soup.select('#inventor'))
+    # find element by css print
+    soup.select('.wow')
+    print("Facebook URL:",soup.find_all('a')[0]['href'])
+    print("Inventor:",soup.find('div',{'id':'inventor'}).text)
+    print("Span content:", soup.select('span')[0].getText())
+
 test2()
