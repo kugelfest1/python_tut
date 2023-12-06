20a39fb22c997164034bc702a1e8eae0cdac4fde
Edward Kan <kan1010@freenet.de>
2023-12-06 21:41:43 +0000

    scrape_myrient() creating definitive rom lists, ok

--- 3kw46 automateit python/a3.py
+++ 3kw46 automateit python/a3.py
@@ -48,8 +48,8 @@ def test2():
         print(url2)
         soup=BeautifulSoup(urllib2.urlopen(urllib2.Request(url2,headers=HEADER)),features="lxml")
         # find all 'a' elements which contain a nonempty "title" attribute
-        zz=soup.find_all("a",{"title":re.compile("\w")})
-        for i in zz:
+        roms=soup.find_all("a",{"title":re.compile("\w")})
+        for i in roms:
             print(i.get('href'),i.get('title'))
         print('-'*80)
 
@@ -69,8 +69,8 @@ def test3():
         print(url2)
         soup=BeautifulSoup(urllib2.urlopen(urllib2.Request(url2,headers=HEADER)),features="lxml")
         # find all 'a' elements which contain a nonempty "title" attribute
-        zz=soup.find_all("a",{"title":re.compile("\w")})
-        for i in zz:
+        roms=soup.find_all("a",{"title":re.compile("\w")})
+        for i in roms:
             print(i.get('href'),i.get('title'))
         print('-'*80)
 
@@ -110,8 +110,8 @@ def test3b():
                 try:
                     soup=BeautifulSoup(urllib2.urlopen(urllib2.Request(url2,headers=HEADER)),features="lxml")
                     # find all 'a' elements which contain a nonempty "title" attribute
-                    zz=soup.find_all("a",{"title":re.compile("\w")})
-                    for i in zz:
+                    roms=soup.find_all("a",{"title":re.compile("\w")})
+                    for i in roms:
                         f.write(url2+i.get('href')+'\n')
                         sys.stdout.write('.')
                 except:
@@ -144,8 +144,8 @@ def myrient_scraper(url,res):
                 try:
                     soup=BeautifulSoup(urllib2.urlopen(urllib2.Request(url2,headers=HEADER)),features="lxml")
                     # find all 'a' elements which contain a nonempty "title" attribute
-                    zz=soup.find_all("a",{"title":re.compile("\w")})
-                    for i in zz:
+                    roms=soup.find_all("a",{"title":re.compile("\w")})
+                    for i in roms:
                         f.write(url2+i.get('href')+'\n')
                         sys.stdout.write('.')
                 except:
@@ -163,7 +163,53 @@ def myrient_scraper_stats(url,res):
     global HEADER
     try:
         f=open(res,"w",buffering=0)    # unbuffered
-        f2=open("stat"+res,"w",buffering=0)    # unbuffered
+#        f2=open("stat"+res,"w",buffering=0)    # unbuffered
+        soup=BeautifulSoup(urllib2.urlopen(urllib2.Request(url, headers=HEADER)),features="lxml")
+        ls=soup.find_all("table",{"id":"list"})
+        # find all 'a' elements which contain a nonempty "title" attribute
+        rigs=ls[0].find_all("a",{"title":re.compile("\w")})
+        for i in rigs:
+    #        print(i.get('href'),i.get('title'))
+    #        print(i.get('href'))
+            url2=url+i.get('href')
+            sys.stdout.write(i.get('title'))
+
+            # try max x times
+            for j in range(5):
+                try:
+                    # FIXME there seems to be a bug in the retry which causes a buildup of duplicates (compare to scrape_myrient())
+                    soup=BeautifulSoup(urllib2.urlopen(urllib2.Request(url2,headers=HEADER)),features="lxml")
+                    # find all 'a' elements which contain a nonempty "title" attribute
+                    roms=soup.find_all("a",{"title":re.compile("\w")})
+                    for k in roms:
+#                        f.write(url2+k.get('href')+'\n')
+                        f.write("%s\t%s\t%s\n" % (k.get('title'),i.get('title'),url2+k.get('href')))
+                        sys.stdout.write('.')
+                except:
+                    sys.stdout.write("-%-retrying.. \n")
+                    time.sleep(2**j)
+                    continue
+                break
+    #        print('-'*80)
+    except:
+        sys.stdout.write("-%-buggered\n")
+
+    f.close()
+#    f2.close()
+
+def test3c():
+    jobs={ 'https://myrient.erista.me/files/No-Intro/': 'nointro.txt',
+        'https://myrient.erista.me/files/Redump/': 'redump.txt'
+    }
+    for k,v in jobs.items():
+        myrient_scraper_stats(k,v)
+
+# return res list of (title, machine, url)
+def scrape_myrient(url,res):
+    global HEADER
+    try:
+#        f=open(res,"w",buffering=0)    # unbuffered
+#        f2=open("stat"+res,"w",buffering=0)    # unbuffered
         soup=BeautifulSoup(urllib2.urlopen(urllib2.Request(url, headers=HEADER)),features="lxml")
         ls=soup.find_all("table",{"id":"list"})
         # find all 'a' elements which contain a nonempty "title" attribute
@@ -179,10 +225,11 @@ def myrient_scraper_stats(url,res):
                 try:
                     soup=BeautifulSoup(urllib2.urlopen(urllib2.Request(url2,headers=HEADER)),features="lxml")
                     # find all 'a' elements which contain a nonempty "title" attribute
-                    zz=soup.find_all("a",{"title":re.compile("\w")})
-                    for k in zz:
-                        f.write(url2+k.get('href')+'\n')
-                        f2.write("%s,%s,%s\n" % (url,i.get('title'),k.get('title')))
+                    roms=soup.find_all("a",{"title":re.compile("\w")})
+                    for k in roms:
+#                        f.write(url2+k.get('href')+'\n')
+#                        f2.write("%s,%s,%s\n" % (url,i.get('title'),k.get('title')))
+                        res.append([k.get('title'),i.get('title'),url2+k.get('href')])
                         sys.stdout.write('.')
                 except:
                     sys.stdout.write("-%-retrying.. \n")
@@ -193,14 +240,21 @@ def myrient_scraper_stats(url,res):
     except:
         sys.stdout.write("-%-buggered\n")
 
-    f.close()
-    f2.close()
-
-def test3c():
-    jobs={ 'https://myrient.erista.me/files/No-Intro/': 'nointro.txt',
-        'https://myrient.erista.me/files/Redump/': 'redump.txt'
+def test3d():
+    jobs={ 'https://myrient.erista.me/files/No-Intro/': 'nointro2.txt',
+        'https://myrient.erista.me/files/Redump/': 'redump2.txt'
     }
     for k,v in jobs.items():
-        myrient_scraper_stats(k,v)
+        res=[]
+        scrape_myrient(k,res)
+        f=open(v,"w")
+        f.write('rom\tmachine\turl\n')
+        for el in res:
+            try:
+                f.write("%s\n" % '\t'.join(map(str,el)))
+            except:
+                sys.stderr.write("Oops\n")
+        f.close()
 
-test3c()
+#test3c()
+test3d()
